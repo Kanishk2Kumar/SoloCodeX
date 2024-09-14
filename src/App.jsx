b67b846b68436b931100ca97d23179181a464c6b
Kanishk <kanishkkumar127@gmail.com>
2024-09-14 13:04:01 +0530

    Added suitable Sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-export default function App() {
+import Nav from "./components/Nav";
+import {
+  Hero,
+  AboutEvent,
+  Prizes,
+  OurSponsors,
+  Register,
+  Footer
+} from "./sections";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <AboutEvent />
+      </section>
+      <section className='padding'>
+        <Prizes />
+      </section>
+      <section className='padding-x py-10'>
+        <OurSponsors />
+      </section>
+      <section className='padding'>
+        <Register />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
   )
 }
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import AboutEvent from "./AboutEvent";
+import OurSponsors from "./OurSponsors";
+import Prizes from "./Prizes";
+import Register from "./Register";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    AboutEvent,
+    OurSponsors,
+    Prizes,
+    Register,
+    Footer
+}
